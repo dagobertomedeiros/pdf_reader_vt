d18dd1a44fcf2e3645a0e0ece317f694df2c43fe
Dagoberto Medeiros <dagobertomedeiros@gmail.com>
2022-03-23 17:12:52 -0300

    arquivo descontinuado

--- read_pdf.js
+++
@@ -1,9 +0,0 @@
-let PdfReader = require("pdfreader").PdfReader;
-let lines = {};
-let rep = null
-new PdfReader().parseFileItems("53867.pdf", function(err, item){
-    if (item && item.text)
-      console.log(item.text);
-      rep = item.text
-  });
-  console.log(rep)
